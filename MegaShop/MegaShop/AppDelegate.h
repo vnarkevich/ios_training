6cc8844908aba2d777a7931fdff41e67466971f6
vnarkevich <v.narkevich@gmail.com>
2014-03-17 11:31:40 +0400

    create project

---
+++ MegaShop/MegaShop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MegaShop
+//
+//  Created by Admin on 17.03.14.
+//  Copyright (c) 2014 SaM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
